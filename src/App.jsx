e49c5caad30eeb8a75a3b8c3b35a754a2d11ea87
Rohan Gope <rg2512@it.jgec.ac.in>
2024-06-12 22:46:16 +0530

    refactor: Add meta tags and update favicon for SEO optimization

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import Form from './components/Form';
 import Show from './components/Show';
 import { DataProvider } from './context/data';
+import { Helmet } from 'react-helmet';
 
 function App() {
 	const [todos, setTodos] = useState([]);
@@ -72,6 +73,42 @@ function App() {
 				deleteAllTodo,
 			}}
 		>
+			<Helmet>
+				<meta charSet='utf-8' />
+				<title>Todo List </title>
+				<meta
+					name='description'
+					content=' A simple Todo List application built using ReactJS. Manage your tasks efficiently with our Todo List application.'
+				/>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1'
+				/>
+				<meta
+					name='author'
+					content='Neha Panda'
+				/>
+				<meta
+					name='keywords'
+					content='Todo List, React, ReactJS, ReactJS Project, React Project, React Todo List, Todo List App, React Todo List App'
+				/>
+				<link
+					rel='icon'
+					href='/logo.png'
+				/>
+				<meta
+					property='og:title'
+					content='Todo List Application'
+				/>
+				<meta
+					property='og:description'
+					content='A simple Todo List application built using ReactJS. Manage your tasks efficiently with our Todo List application.'
+				/>
+				<meta
+					property='og:image'
+					content='/logo.png'
+				/>
+			</Helmet>
 			<div className='min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-gray-800 via-gray-900 to-black'>
 				<div className='flex flex-col gap-4 bg-[#333A49] md:w-1/2 border-zinc-300 border-2 justify-center items-center rounded-xl p-8 shadow-lg shadow-[#ffffff2b]'>
 					<h1 className='text-4xl font-serif text-[#B3C5EF] mb-4'>Todo App</h1>
